8c99df3d267f473f3f2e1226d87983901410b653
Thiry Prune <prune.thiry@hotmail.com>
2025-10-27 16:45:30 +0100

    feat: add heatmap

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('./public'));
 
-app.get('/api/rootme', async (req, res) => {
-  const response = await fetch("https://api.www.root-me.org/auteurs/1033656", {
-    headers: { 
-        'Cookie': `api_key=${process.env.ROOTME_API_KEY}`,
-        'User-Agent': 'curl/8.5.0'
-    }
-  });
-  const data = await response.json();
-  res.json(data);
-});
+// app.get('/api/rootme', async (req, res) => {
+//   const response = await fetch("https://api.www.root-me.org/auteurs/1033656", {
+//     headers: { 
+//         'Cookie': `api_key=${process.env.ROOTME_API_KEY}`,
+//         'User-Agent': 'curl/8.5.0'
+//     }
+//   });
+//   const data = await response.json();
+//   res.json(data);
+// });
 
 app.listen(3000, () => console.log('Serveur démarré sur http://localhost:3000'));
---
+++ public/js/heatmap.js
@@ -0,0 +1,121 @@
+// set the dimensions and margins of the graph
+var margin = {top: 80, right: 5, bottom: 30, left: 50},
+  width = 600 - margin.left - margin.right,
+  height = 270 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("../day-to-day-data.csv", function(data) {
+
+  // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+  var myWeeks = d3.map(data, function(d){return d.Semaine;}).keys()
+  var myDay = d3.map(data, function(d){return d.Jour;}).keys()
+
+  // Build X scales and axis:
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(myWeeks)
+    .padding(0.05);
+
+  // Build Y scales and axis:
+  var y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(myDay)
+    .padding(0.05);
+  svg.append("g")
+    .style("font-size", 15)
+    .call(d3.axisLeft(y).tickSize(0))
+    .select(".domain").remove()
+
+  // Build color scale
+  var myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateBuPu)
+    .domain([0,4])
+
+  // create a tooltip
+  var tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "rgba(255, 255, 255, 0.7)")
+    .style("border-radius", "8px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      .style("stroke", "white")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    tooltip
+      .html(`<p style="text-decoration: underline">${d.Date}&nbsp;:</p><ul><li>${d.Activity1}</li><li>${d.Activity2}</li><li>${d.Activity3}</li><li>${d.Activity4}</li></ul>`)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", d => {
+            const [day, month, year] = d.Date.split("/");
+            return new Date(year, month - 1, day).setHours(0,0,0,0) === new Date().setHours(0,0,0,0) ? "var(--neon-blue)" : "none";
+            })
+      .style("opacity", 0.8)
+  }
+
+  // add the squares
+  svg.selectAll()
+    .data(data, function(d) {return d.Semaine+':'+d.Jour;})
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.Semaine) })
+      .attr("y", function(d) { return y(d.Jour) })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("width", 20 )
+      .attr("height", 20 )
+      .style("fill", function(d) { return myColor(d.ActivityNb)} )
+      .style("stroke-width", 4)
+      .style("stroke", d => {
+            const [day, month, year] = d.Date.split("/");
+            return new Date(year, month - 1, day).setHours(0,0,0,0) === new Date().setHours(0,0,0,0) ? "var(--neon-blue)" : "none";
+            })
+      .style("opacity", 0.8)
+      .style("margin", "2px")
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
+})
+
+console.log(new Date("25/09/2025"), new Date());
+
+// Add title to graph
+svg.append("text")
+        .attr("x", 0)
+        .attr("y", -50)
+        .attr("text-anchor", "left")
+        .style("font-size", "22px")
+        .style("text-align", "center")
+        .text("Day-to-day Activities");
+
+// Add subtitle to graph
+svg.append("text")
+        .attr("x", 0)
+        .attr("y", -20)
+        .attr("text-anchor", "left")
+        .style("font-size", "14px")
+        .style("fill", "grey")
+        .style("max-width", 400)
+        .style("text-align", "center")
+        .text("Hover to know on what I've been working since the beginning of the vocational training.");
